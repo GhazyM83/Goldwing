e14583346031fa0bfeb7e0b622c037863ba5779c
GhazyM83 <Iamghazy83@gmail.com>
2021-11-24 22:57:12 -0800

    Added login and signup activities. Connected to Firebase and added FirebaseServices.

--- app/src/main/java/com/ghazy/goldwing/MainActivity.java
+++ app/src/main/java/com/ghazy/goldwing/MainActivity.java
@@ -2,8 +2,11 @@ package com.ghazy.goldwing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+    }
+
+    public void goToLogin(View view) {
+        Intent i = new Intent(getApplicationContext(), Login.class);
+        startActivity(i);
+    }
+
+    public void goToSignup(View view) {
+        Intent i = new Intent(getApplicationContext(), Signup.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/ghazy/goldwing/Signup.java
@@ -0,0 +1,15 @@
+package com.ghazy.goldwing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        getSupportActionBar().hide();
+    }
+}
